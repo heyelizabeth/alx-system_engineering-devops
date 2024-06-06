fb29838dedfb4b9c493cf942e7a6ea26094b7212
heyelizabeth <helloaleeyah@gmail.com>
2024-06-06 16:45:44 -0700

    updated

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""Query Reddit API to determine subreddit sub count
-"""
+"first 10 hot posts"
 
 import requests
 
 
 def top_ten(subreddit):
-    """Request top ten hot posts of subreddit
-    from Reddit API
-    """
-    # set custom user-agent
-    user_agent = '0x16-api_advanced-jmajetich'
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-
-    # custom user-agent avoids request limit
-    headers = {'User-Agent': user_agent}
-
-    r = requests.get(url, headers=headers, allow_redirects=False)
-
-    if r.status_code != 200:
-        print('None')
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        for i in range(10):
+            print(data['data']['children'][i]['data']['title'])
     else:
-        # load response unit from json
-        data = r.json()['data']
-        # extract list of pages
-        posts = data['children']
-        for post in posts:
-            print(post['data']['title'])
+        print(None)
